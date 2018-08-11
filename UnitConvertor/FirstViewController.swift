5c6d7b20be88651afdd9142237312300cafbf2e3
Shalitha Mihiranga <shalithamihiranga@shalithas-macbook-pro.local>
2018-08-11 12:35:22 +0530

    Initial Commit

---
+++ UnitConvertor/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UnitConvertor
+//
+//  Created by Shalitha Mihiranga on 8/11/18.
+//  Copyright © 2018 Shalitha Mihiranga. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UnitConvertor/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UnitConvertor
+//
+//  Created by Shalitha Mihiranga on 8/11/18.
+//  Copyright © 2018 Shalitha Mihiranga. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
